0d4f73763ce57918ac8a378c0571f95145bf2d79
swem <r97922120@ntu.edu.tw>
2014-01-14 00:42:37 +0800

    rails g model post content:text group_id:integer and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140113132649) do
+ActiveRecord::Schema.define(:version => 20140113164154) do
 
   create_table "groups", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.text     "content"
+    t.integer  "group_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
