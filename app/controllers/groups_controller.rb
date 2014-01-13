ce9cc1f0b258368f129b704ebe9c5eaf66aaaa35
swem <r97922120@ntu.edu.tw>
2014-01-13 23:07:46 +0800

    complete restful actions for groups
    add simple_form gem, use simple_form in views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,41 @@
 class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(params[:group])
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update_attributes(params[:group])
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path
+  end
 end
